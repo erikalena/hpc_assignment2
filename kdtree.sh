44b59c0c25d2cc2c97b30dc6052a82d5587c60b7
erikalena <lena.erika@gmail.com>
2022-02-19 16:16:09 +0100

    updated bash

--- kdtree.sh
+++ kdtree.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PBS -l nodes=1:ppn=48
+#PBS -l nodes=1:ppn=24
 #PBS -l walltime=00:30:00
 #PBS -q dssc_gpu
 
@@ -17,23 +17,28 @@ printf '%s,%s,%s,\n' 'n_procs' 'n_threads' 'time_taken (us)' > performances.csv
 
 # establish the maximum number of processors on which you want to test the code
 # provide it as the maximum p s.t. 2^p = n. of processors
-p=5
+p=4
 
-# set number of threads o be used
-n_threads=2
-export OMP_NUM_THREADS=${n_threads}
+n_procs=24
 export OMP_PLACES=cores
 
 
 for i in  $( seq 0 $p )
 do
     let n=(2**$i)
-    mpirun --mca btl ^openib -np $n ./kdtree > time_taken.txt
- 
-    times[i]=$(cat time_taken.txt | cut -f2 -d ':')  
+    let t=($n_procs/$n)
+    for j in  $( seq 1 $t )
+    do  
+        export OMP_NUM_THREADS=$j
+        mpirun --mca btl ^openib -np $n --map-by socket ./kdtree > time_taken.txt
+     
+        times[i]=$(cat time_taken.txt | cut -f2 -d ':')  
 
-    printf '%s,%s,%s,\n' ${n} ${n_threads} ${times[i]} >> performances.csv 
+        printf '%s,%s,%s,\n' ${n} ${j} ${times[i]} >> performances.csv 
+    done
 done
+
+export OMP_PLACES=cores
 
 
 exit
--- prova.sh
+++ prova.sh
@@ -7,24 +7,26 @@ printf '%s,%s,%s,\n' 'n_procs' 'n_threads' 'time_taken (us)' > performances.csv
 
 # establish the maximum number of processors on which you want to test the code
 # provide it as the maximum p s.t. 2^p = n. of processors
-p=4
+p=3
 
 # set number of threads o be used
-n_threads=2
-export OMP_NUM_THREADS=${n_threads}
+n_procs=16
 export OMP_PLACES=cores
-
 
 for i in  $( seq 0 $p )
 do
     let n=(2**$i)
-    mpirun -np $n ./kdtree > time_taken.txt
- 
-    times[i]=$(cat time_taken.txt | cut -f2 -d ':')  
+    let t=($n_procs/$n)
+    for j in  $( seq 1 $t )
+    do  
+        export OMP_NUM_THREADS=$j
+        mpirun -np $n ./kdtree > time_taken.txt
+     
+        times[i]=$(cat time_taken.txt | cut -f2 -d ':')  
 
-    printf '%s,%s,%s,\n' ${n} ${n_threads} ${times[i]} >> performances.csv 
+        printf '%s,%s,%s,\n' ${n} ${t} ${times[i]} >> performances.csv 
+    done
+    echo $n $t
 done
-
-
 
 
