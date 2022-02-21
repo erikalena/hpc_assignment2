fd3b03afa60d80cd45ee8ad5b13ce738433e7ee7
erikalena <lena.erika@gmail.com>
2022-02-21 12:49:09 +0100

    prova

--- sorting_data.c
+++ sorting_data.c
@@ -1,9 +1,9 @@
 #include "sorting_data.h"
 
-//process and sort data with respect to the given axis
+#define SWAP(A,B,SIZE) do {int sz = (SIZE); char *a = (A); char *b = (B); \
+    do { char _temp = *a;*a++ = *b;*b++ = _temp;} while (--sz);} while (0)
+
 int sorting(data_t* data, int npoints, int axis) {
-    
-  int mid;
 
   #if defined(DEBUG)
     #if defined(_OPENMP)
@@ -11,66 +11,27 @@ int sorting(data_t* data, int npoints, int axis) {
     printf("%d threads working, start partitioning the array..\n", nthreads);
     #endif
   #endif
-  mid = partitioning( data, 0, npoints, axis, compare_ge);
+
   
-  return mid;
-}
-
-int find_median(data_t *data, int start, int end, int dim) {
-    float_t min = MAX_VALUE, max = MIN_VALUE;
-     
-    #if defined(_OPENMP)
-        #pragma omp parallel
-        {
-	        #pragma omp for reduction (max:max)
-	        for(int i = start; i < end; i++) 
-                max = data[i].data[dim] > max ? data[i].data[dim] : max;
-
-            #pragma omp for reduction (min:min)
-	        for(int i = start; i < end; i++) 
-                min = data[i].data[dim] < min ? data[i].data[dim] : min;
-        }
-	#else
-        for(int i = start; i < end; i++) {
-            max = data[i].data[dim] > max ? data[i].data[dim] : max;
-            min = data[i].data[dim] < min ? data[i].data[dim] : min;
-        }
-    #endif
-    // find element which is closest to the median
-    float_t median = (max - min)/2;
-    int pivot = 0;
-    for(int i = start; i < end; i++) {
-       pivot = (abs(data[i].data[dim] - median) <= abs(data[pivot].data[dim] - median)) ? i : pivot;
-    }
-    return pivot; 
-
-}
-
-#define SWAP(A,B,SIZE) do {int sz = (SIZE); char *a = (A); char *b = (B); \
-    do { char _temp = *a;*a++ = *b;*b++ = _temp;} while (--sz);} while (0)
-
-inline int partitioning( data_t *data, int start, int end, int dim, compare_t cmp_ge )
-{
-	
-	
+  int start = 0, end = npoints, dim = axis;
   // pick up the closest to the middle as pivot 
   int median = find_median(data, start, end, dim);
   
   //put pivot in last position
   SWAP( (void*)&data[median], (void*)&data[end-1], sizeof(data_t) ); 
-  void *pivot = (void*)&data[end-1];
+  int pivot = end-1;
   
   int pointbreak = end-1;
   for ( int i = start; i <= pointbreak; i++ )
-    if( cmp_ge( (void*)&data[i], pivot, dim) ) {
-	    while( (pointbreak > i) && cmp_ge( (void*)&data[pointbreak], pivot, dim) ) 
+    if( data[i].data[dim] >= data[pivot].data[dim] ) {
+	    while( (pointbreak > i) && data[i].data[dim] >= data[pointbreak].data[dim] ) 
 	        pointbreak--;
 	    if (pointbreak > i ) 
 	        SWAP( (void*)&data[i], (void*)&data[pointbreak--], sizeof(data_t) );
     }  
     
-  pointbreak += !cmp_ge( (void*)&data[pointbreak], pivot, dim) ;
-  SWAP( (void*)&data[pointbreak], pivot, sizeof(data_t) ); 
+  pointbreak += !(data[pointbreak].data[dim] >= data[pivot].data[dim]) ;
+  SWAP( (void*)&data[pointbreak], (void*)&data[pivot], sizeof(data_t) ); 
   
 	#if defined(DEBUG)
 	#define CHECK {							\
@@ -86,6 +47,58 @@ inline int partitioning( data_t *data, int start, int end, int dim, compare_t cm
   return pointbreak;
 }
 
+int find_median(data_t *data, int start, int end, int dim) {
+    // find max and min
+    float_t min = MAX_VALUE, max = MIN_VALUE;
+     
+    #if defined(_OPENMP)
+        #pragma omp parallel
+        {
+	        #pragma omp for reduction (max:max) reduction(min:min)
+	        for(int i = start; i < end; i++) {
+                max = data[i].data[dim] > max ? data[i].data[dim] : max;
+                min = data[i].data[dim] < min ? data[i].data[dim] : min;
+            }
+        }
+	#else
+        for(int i = start; i < end; i++) {
+            max = data[i].data[dim] > max ? data[i].data[dim] : max;
+            min = data[i].data[dim] < min ? data[i].data[dim] : min;
+        }
+    #endif
+    
+    // find element which is closest to the median
+    float_t median = (max - min)/2;
+    int pivot = 0;
+    min = MAX_VALUE;
+    #if defined(_OPENMP)
+        #pragma omp parallel
+        {
+            int index_local = 0;
+            float_t min_local = MAX_VALUE;  
+            #pragma omp for 
+            for (int i = start; i < end; i++) {        
+                if (fabs(data[i].data[dim] - median) < min_local) {
+                    min_local = fabs(data[i].data[dim] - median);
+                    index_local = i;
+                }
+            }
+            #pragma omp critical 
+            {
+                if (min_local < min) {
+                    min = min_local;
+                    pivot = index_local;
+                }
+            }
+        }
+    #else 
+        for(int i = start; i < end; i++) {
+           pivot = (abs(data[i].data[dim] - median) <= abs(data[pivot].data[dim] - median)) ? i : pivot;
+        }
+    #endif
+    return pivot; 
+
+}
 
 
 
@@ -124,7 +137,7 @@ int show_array( data_t *data, int start, int end, int dim, int not_used ) {
 }
 
 
-inline int compare( const void *A, const void *B, int dim ) {
+int compare( const void *A, const void *B, int dim ) {
   data_t *a = (data_t*)A;
   data_t *b = (data_t*)B;
 
@@ -132,9 +145,4 @@ inline int compare( const void *A, const void *B, int dim ) {
   return ( (diff > 0) - (diff < 0) );
 }
 
-inline int compare_ge( const void *A, const void *B, int dim ) {
-  data_t *a = (data_t*)A;
-  data_t *b = (data_t*)B;
 
-  return (a->data[dim] >= b->data[dim]);
-}
